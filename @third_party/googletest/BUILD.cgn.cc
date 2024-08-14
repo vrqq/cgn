49071e536c223f52560a0d9a5dd8df49c063c9a9
vrqq <vrqq3118@163.com>
2024-08-14 23:32:21 +0800

    add @third_party//re2

--- @third_party/googletest/BUILD.cgn.cc
+++ @third_party/googletest/BUILD.cgn.cc
@@ -3,12 +3,11 @@
 git("googletest.git", x) {
     x.repo = "https://github.com/google/googletest.git";
     x.commit_id = "2954cb8d879886403d55343f941ae7d0216e0f6b";
+    x.dest_dir = "repo";
 }
 
-
+const static std::string gtest = "repo/googletest";
 cxx_shared("gtest", x) {
-    const static std::string gtest = "src/googletest";
-
     x.pub.include_dirs = {gtest + "/include"};
     x.pub.defines = {
         "GTEST_LINKED_AS_SHARED_LIBRARY=1", "GTEST_ENABLE_CATCH_EXCEPTIONS_=1"
@@ -18,15 +17,22 @@ cxx_shared("gtest", x) {
     x.include_dirs = {gtest, gtest + "/include"};
     x.srcs = {gtest + "/src/gtest-all.cc"};
 }
+cxx_sources("gtest_main", x) {
+    x.srcs = {gtest + "/src/gtest_main.cc"};
+    x.add_dep(":gtest", cxx::inherit);
+}
 
+const static std::string gmock = "repo/googlemock";
 cxx_shared("gmock", x) {
-    const static std::string gmock = "src/googlemock";
-
     x.pub.include_dirs = {gmock + "/include"};
     x.defines = {"GTEST_CREATE_SHARED_LIBRARY=1"};
     x.include_dirs = {gmock, gmock + "/include"};
     x.srcs = {gmock + "/src/gmock-all.cc"};
     x.add_dep(":gtest", cxx::inherit);
+}
+cxx_sources("gmock_main", x) {
+    x.srcs = {gmock + "/src/gmock_main.cc"};
+    x.add_dep(":gmock", cxx::inherit);
 }
 
 // group("googletest", x) {
@@ -47,8 +53,8 @@ alias("googletest", x) {
 // }
 bin_devel("devel", x) {
     x.include = {
-        {"src/googlemock/include", {"gmock/*.h"}},
-        {"src/googletest/include", {"gtest/*.h"}}
+        {gmock + "/include", {"gmock/*.h"}},
+        {gtest + "/include", {"gtest/*.h"}}
     };
     x.add_from_target(":googletest", x.allow_linknrun);
 }
