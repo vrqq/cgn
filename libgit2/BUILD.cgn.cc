fa0d1d6b9fd9baf3626ed791e56402c35563a031
vrqq <vrqq3118@163.com>
2025-01-03 22:24:11 +0800

    v0.999999 cgn.exe on windows platform almost done

--- libgit2/BUILD.cgn.cc
+++ libgit2/BUILD.cgn.cc
@@ -1,7 +1,13 @@
 #include "cgn"
 
+git("libgit2.git", x) {
+    x.repo = "https://github.com/libgit2/libgit2.git";
+    x.commit_id = "338e6fb681369ff0537719095e22ce9dc602dbf0"; // v1.9.0
+    x.dest_dir = "repo";
+}
+
 cmake("libgit2", x) {
-    x.sources_dir = "src";
-    x.outputs = {"bin/git2", "lib64/libgit2.so", "lib64/libgit2.so.1.8", 
-                 "lib64/libgit2.so.1.8.0"};
+    x.sources_dir = "repo";
+    x.outputs = {"bin/git2", "lib64/libgit2.so", "lib64/libgit2.so.1.9", 
+                 "lib64/libgit2.so.1.9.0"};
 }
