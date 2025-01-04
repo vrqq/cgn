e5e55a194186abbed67b5311e5a5a7da85b97b00
vrqq <vrqq3118@163.com>
2025-01-04 22:53:20 +0800

    update graph bug, add asan on cxx_interpreter

--- sqlite3/BUILD.cgn.cc
+++ sqlite3/BUILD.cgn.cc
@@ -5,6 +5,11 @@ git("sqlite3.git", x) {
     x.commit_id = "b74eb00e2cb05d9749859e6fbe77d229ad1dc1e1";
 }
 
+url_download("sqlite3.https", x) {
+    x.url = "https://www.sqlite.org/2024/sqlite-amalgamation-3470200.zip";
+    x.outputs = {"sqlite-amalgamation-3470200.zip"};
+}
+
 cxx_shared("sqlite3", x) {
     x.srcs = {"repo/sqlite3.c"};
     x.pub.include_dirs = {"src"};
