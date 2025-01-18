e67d8fc236ecc299a6ab76696f38caf789128fc3
vrqq <vrqq3118@163.com>
2025-01-19 00:24:52 +0800

    @third_party//boringssl updated

--- boringssl/BUILD.cgn.cc
+++ boringssl/BUILD.cgn.cc
@@ -1,3 +1,4 @@
+// 
 #include <cgn>
 #include <@third_party/nasm/nasm.cgn.h>
 #include "sources.cgn.h"
@@ -28,19 +29,15 @@ std::vector<std::string> src_prefix(const std::vector<std::string> &in) {
     return rv;
 }
 
-static std::vector<std::string> all_sources
-    = src_prefix(bcm_internal_headers + bcm_sources + crypto_internal_headers +
-                 crypto_sources + ssl_internal_headers + ssl_sources + pki_sources);
-
-
-nasm_object("boringssl_asm", x) {
-    x.srcs = src_prefix(bcm_sources_nasm + crypto_sources_nasm);
+bool is_msan(cxx::CxxContext &x) {
+    return (x.cfg["cxx_msan"] != "");
 }
-
-cxx_static("boringssl", x) {
-    x.srcs = all_sources;
-    x.pub.include_dirs = x.include_dirs = {"repo/include"};
-    x.defines = {"BORINGSSL_IMPLEMENTATION", "OPENSSL_SMALL"};
+void apply_build_setting(cxx::CxxContext &x, bool for_test = false) {
+    x.pub.include_dirs += src_prefix({"include"});
+    x.include_dirs     += src_prefix({"include"});
+    if (!for_test)
+        x.defines += {"BORINGSSL_IMPLEMENTATION"};
+    x.defines += {"OPENSSL_SMALL"};
     if (x.cfg["cxx_toolchain"] == "msvc") {
         x.cflags += {
             "/wd4100", 
@@ -51,10 +48,13 @@ cxx_static("boringssl", x) {
             "/wd4706",
         };
     }
+    if (is_msan(x))
+        x.pub.defines += {"OPENSSL_NO_ASM"};
 
     if (x.cfg["os"] == "win") {
         x.defines += {"_HAS_EXCEPTIONS=0", "NOMINMAX", 
-            "WIN32_LEAN_AND_MEAN", "_CRT_SECURE_NO_WARNINGS"};
+            "WIN32_LEAN_AND_MEAN", "_CRT_SECURE_NO_WARNINGS",
+        };
     }
     
     if (x.cfg["os"] == "linux") {
@@ -64,7 +64,84 @@ cxx_static("boringssl", x) {
         if (x.cfg["cxx_toolchain"] == "llvm")
             x.cflags += {"-Wctad-maybe-unsupported",
                          "-Wframe-larger-than=25344"};
+        
     }
-    x.add_dep(":boringssl_asm", cxx::private_dep);
+} //apply_build_setting()
 
+// for windows using nasm (only use when is_msan()==false)
+// for linux using gcc / clang instead
+nasm_object("_crypto_asm_msvc", x) {
+    x.srcs = src_prefix(bcm_sources_nasm + crypto_sources_nasm);
+}
+
+// -- The first form: libssl.a, libcrypto.a, (internal) libpki.a
+cxx_static("ssl", x) {
+    x.srcs = src_prefix(ssl_sources);
+    apply_build_setting(x);
+    x.add_dep(":crypto", cxx::inherit);
+}
+
+cxx_static("crypto", x) {
+    x.srcs = src_prefix(bcm_sources + crypto_sources);
+    apply_build_setting(x);
+    if (is_msan(x) == false) {
+        if (x.cfg["cxx_toolchain"] == "msvc")
+            x.add_dep(":_crypto_asm_msvc", cxx::private_dep | cxx::pack_obj);
+        else
+            x.srcs += src_prefix(bcm_sources_asm + crypto_sources_asm);
+    }
+}
+
+cxx_static("pki", x) {
+    x.srcs = src_prefix(pki_sources);
+    apply_build_setting(x);
+    x.add_dep(":crypto", cxx::inherit);
+}
+
+cxx_executable("bssl", x) {
+    x.srcs = src_prefix(bssl_sources);
+    apply_build_setting(x);
+    x.add_dep(":ssl", cxx::private_dep);
+    x.add_dep(":crypto", cxx::private_dep);
+    x.add_dep(":pki", cxx::private_dep);
+}
+
+// test suite
+nasm_object("_test_support_msvc", x) {
+    x.srcs = src_prefix(test_support_sources_nasm);
+}
+cxx_sources("_test_support", x) {
+    x.srcs = src_prefix(test_support_sources);
+    if (x.cfg["cxx_toolchain"] != "msvc")
+        x.srcs += src_prefix(test_support_sources_asm);
+    apply_build_setting(x);
+    // x.add_dep(":_test_support_asm", cxx::inherit);
+    x.add_dep("@third_party//googletest", cxx::inherit);
+}
+cxx_executable("ssl_test", x) {
+    x.srcs = src_prefix(ssl_test_sources);
+    x.add_dep(":ssl", cxx::private_dep);
+    x.add_dep(":crypto", cxx::private_dep);
+    x.add_dep(":_test_support", cxx::private_dep);
+}
+
+// -- The second form: libboringssl.a
+cxx_static("libboringssl", x)
+{
+    x.srcs = src_prefix(
+        bcm_internal_headers + bcm_sources + crypto_internal_headers +
+        crypto_sources + ssl_internal_headers + ssl_sources + pki_sources);
+    apply_build_setting(x);
+    
+    if (is_msan(x) == false) {
+        if (x.cfg["cxx_toolchain"] == "msvc")
+            x.add_dep(":_crypto_asm", cxx::private_dep);
+        else
+            x.srcs += src_prefix(bcm_sources_asm + crypto_sources_asm);
+    }
+    if (x.cfg["os"] == "win")
+        x.perferred_binary_name = "boringssl.lib";
+    else
+        x.perferred_binary_name = "libboringssl.a";
+    
 }
