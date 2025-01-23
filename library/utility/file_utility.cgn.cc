9d48bd75a377005b886a1d377e151359429f3f60
vrqq <vrqq3118@163.com>
2025-01-23 20:52:07 +0800

    @cgn.d//library/utility/file_utility updated

---
+++ library/utility/file_utility.cgn.cc
@@ -0,0 +1,221 @@
+#define CGN_UTILITY_IMPL
+#include <fstream>
+#include "../general.cgn.bundle/bin_devel.cgn.h"
+#include "file_utility.cgn.h"
+
+namespace {
+    struct IWorker {
+        virtual void config(FileUtility &x) = 0;
+        virtual void gen(FileUtility &x, cgn::CGNTargetOpt *opt) = 0;
+    };
+
+    struct CopyWorker : IWorker
+    {
+        std::string cprule;
+        virtual void config(FileUtility &x) {
+            
+            cprule = (x.cfg["host_os"] == "win"? "win_cp_to_dir" : "unix_cp_to_dir");
+        }
+        virtual void gen(FileUtility &x, cgn::CGNTargetOpt *opt) {
+        }
+    }; //struct CopyWorker
+
+    struct BinDevelWorker : IWorker
+    {
+        virtual void config(FileUtility &x) {
+        }
+        virtual void gen(FileUtility &x, cgn::CGNTargetOpt *opt) {
+        }
+    }; //struct BinDevelWorker
+
+    std::string to_working_root(cgn::CGNTargetOpt *opt, const cgn::CGNPath &it) {
+        if (it.type == it.BASE_ON_OUTPUT)
+            return api.rebase_path(it.rpath, ".", opt->out_prefix);
+        else if (it.type == it.BASE_ON_SCRIPT_SRC)
+            return api.rebase_path(it.rpath, ".", opt->src_prefix);
+        return it.rpath;
+    }
+
+} //namespace
+
+void FileUtility::copy_on_build(
+    const std::vector<std::string> &src, 
+    const cgn::CGNPath &src_base, 
+    const cgn::CGNPath &dst_dir
+) {
+    constexpr static char CMD_COPY[] = "copy_to_dir";
+
+    auto gen = [=](cgn::CGNTargetOpt *opt) {
+        std::vector<std::string> rv;
+        for (auto it : src)
+            rv.push_back(it);
+        rv.push_back(to_working_root(opt, src_base));
+        rv.push_back(to_working_root(opt, dst_dir));
+        return rv;
+    };
+    copy_records.push_back(CopyRecord{CMD_COPY, src_base.rpath + "=>" + dst_dir.rpath, gen});
+}
+
+
+void FileUtility::flat_copy_on_build(
+    const std::vector<cgn::CGNPath> &src_list, 
+    const cgn::CGNPath &dst_dir
+) {
+    constexpr static char CMD_FLAT_COPY[] = "flat_copy_to_dir";
+    auto gen = [=](cgn::CGNTargetOpt *opt) {
+        std::vector<std::string> rv;
+        for (auto it : src_list)
+            rv.push_back(to_working_root(opt, it));
+        rv.push_back(to_working_root(opt, dst_dir));
+        return rv;
+    };
+    copy_records.push_back(CopyRecord{CMD_FLAT_COPY, dst_dir.rpath, gen});
+}
+
+// TargetDir
+// include: tgt.h
+//     bin: tgt_win.dll  tgt_win.exe  tgt_linux
+//   lib64: tgt_linux.so  tgt_win.lib
+cgn::CGNTarget FileUtility::collect_devel_on_build(
+    const std::string &label, 
+    BinDevelOpt devel_setting
+) {
+    auto early = opt->quick_dep(label, cfg, true);
+    if (early.errmsg.size())
+        return early;
+
+    if (devel_setting.perferred_libdir.empty()) {
+        if (early.trimmed_cfg["cpu"] == "x86_64")
+            devel_setting.perferred_libdir = "lib64";
+        else
+            devel_setting.perferred_libdir = "lib";
+    }
+
+    if (devel_setting.allow_bindevel){
+        BinDevelInfo *info = early.get<BinDevelInfo>(false);
+        if (info) {
+            copy_on_build(
+                {"*"}, cgn::make_path_base_working(info->base), 
+                cgn::make_path_base_out("."));
+        }
+    }
+
+    if (devel_setting.allow_cxxinclude){
+        cxx::CxxInfo *info = early.get<cxx::CxxInfo>(false);
+        if (info) {
+            for (auto incdir : info->include_dirs)
+                copy_on_build(
+                    {"*"}, cgn::make_path_base_working(incdir), 
+                    cgn::make_path_base_out("include"));
+        }
+    }
+
+    // LinkAndRunInfo::shared_files => lib64
+    // LinkAndRunInfo::static_files => lib64
+    // LinkAndRunInfo::runtime_files => bin
+    if (devel_setting.allow_linknrun) {
+        cgn::LinkAndRunInfo *info = early.get<cgn::LinkAndRunInfo>(false);
+        if (info) {
+            std::vector<cgn::CGNPath> solibs, exes;
+            for (auto so : info->shared_files)
+                solibs.push_back(cgn::make_path_base_working(so));
+            for (auto a : info->static_files)
+                solibs.push_back(cgn::make_path_base_working(a));
+            for (auto exe : info->runtime_files)
+                exes.push_back(cgn::make_path_base_working(exe.second));
+            flat_copy_on_build(solibs, cgn::make_path_base_out(devel_setting.perferred_libdir));
+            flat_copy_on_build(exes, cgn::make_path_base_out(devel_setting.perferred_libdir));
+        }
+    }
+    
+    // .exe .dll => bin
+    // .so .a .lib => lib64
+    if (devel_setting.allow_output) {
+        std::vector<cgn::CGNPath> solibs, exes;
+        for (auto it : early.outputs) {
+            auto ext = api.extension_of_path(it);
+            if (ext == ".a" || ext == ".so" || ext == ".lib")
+                solibs.push_back(cgn::make_path_base_working(it));
+            else if (ext == ".exe" || ext == ".dll")
+                exes.push_back(cgn::make_path_base_working(it));
+        }
+        flat_copy_on_build(solibs, cgn::make_path_base_out(devel_setting.perferred_libdir));
+        flat_copy_on_build(exes, cgn::make_path_base_out(devel_setting.perferred_libdir));
+    }
+
+    result_have_bin_devel = true;
+
+    return early;
+} //FileUtility::collect_devel_on_build()
+
+
+// Using c++17 compiled copy helper
+//  rule advcopy
+//    command = ${exe} -MD ${out}.d --stamp ${out} ${argfile}
+//  build <id>.stamp : adv_copy
+//    exe = ""
+//    cmd = "flat_copy_to_dir"
+//    args = arg1 $
+//           arg2
+void FileUtilityInterpreter::interpret(context_type &x)
+{
+    // copy rule configuration
+    cgn::CGNTarget advcopy = x.opt->quick_dep_namedcfg("@cgn.d//advcopy", "host_release", true);
+    if (advcopy.errmsg.size()) {
+        x.opt->confirm_with_error("Cannot load advcopy: " + advcopy.errmsg);
+        return ;
+    }
+
+    // confirm
+    cgn::CGNTargetOpt *opt = x.opt->confirm();
+    if (opt->cache_result_found)
+        return ;
+
+    // Bindevel postprocess
+    if (x.result_have_bin_devel) {
+        auto *info = opt->result.get<BinDevelInfo>();
+        info->base = opt->out_prefix;
+        info->bin_dir = info->base + "bin";
+        info->lib_dir = info->lib_dir + "lib";
+    }
+
+    // return if no file need update
+    if (opt->file_unchanged)
+        return ;
+
+    // include advcp.ninja
+    std::string rulepath = api.get_filepath("@cgn.d//library/utility/advcp.ninja");
+    opt->ninja->append_include(rulepath);
+
+    // generate ninja file
+    std::vector<std::string> cpstamps_njesc;
+    for (std::size_t i=0; i<x.copy_records.size(); i++) {
+        auto &rec = x.copy_records[i];
+
+        // generate copy_<i>.rsp
+        std::string argfile_path = opt->out_prefix + "copy_" + std::to_string(i) + ".rsp";
+        std::ofstream argfile(argfile_path);
+        for (const auto &arg : rec.arg_gen(opt))
+            argfile << arg << "\n";
+        argfile.close();
+
+        // generate ninja target copy_<i>.stamp
+        auto *field = opt->ninja->append_build();
+        field->rule = "advcopy";
+        field->variables["subcmd"] = rec.command;
+        field->variables["desc"] = std::string{rec.command} + " " + rec.desc;
+        field->variables["exe"]  = opt->ninja->escape_path(advcopy.outputs[0]);
+        field->implicit_inputs   = opt->quickdep_ninja_full;
+        field->implicit_inputs  += {field->variables["exe"]};
+        field->order_only        = opt->quickdep_ninja_dynhdr;
+        field->inputs  = {opt->ninja->escape_path(argfile_path)};
+        field->outputs = {opt->ninja->escape_path(opt->out_prefix + "copy_" + std::to_string(i) + ".stamp")};
+        cpstamps_njesc.push_back(field->outputs[0]);
+    }
+
+    // phony
+    auto *phony = opt->ninja->append_build();
+    phony->rule = "phony";
+    phony->inputs = cpstamps_njesc;
+    phony->outputs = {opt->ninja->escape_path(opt->out_prefix + opt->BUILD_ENTRY)};    
+}
---
+++ library/utility/file_utility.cgn.h
@@ -0,0 +1,126 @@
+#pragma once
+#ifdef _WIN32
+    #ifdef CGN_UTILITY_IMPL
+        #define CGN_UTILITY_API  __declspec(dllexport)
+    #else
+        #define CGN_UTILITY_API
+    #endif
+#else
+    #define CGN_UTILITY_API __attribute__((visibility("default")))
+#endif
+
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <cgn>
+
+namespace cgnv1 {
+
+struct CGNPath {
+
+    constexpr static char BASE_ON_OUTPUT      = 0;
+    constexpr static char BASE_ON_SCRIPT_SRC  = 1;
+    constexpr static char BASE_ON_WORKINGROOT = 2;
+    char type = 2;
+
+    std::string rpath;
+
+    CGNPath(char t, const std::string &rel) : type(t), rpath(rel) {}
+};
+
+inline CGNPath make_path_base_out(const std::string rel="")  { 
+    return CGNPath{CGNPath::BASE_ON_OUTPUT, rel};
+}
+inline CGNPath make_path_base_script(const std::string rel="")  { 
+    return CGNPath{CGNPath::BASE_ON_SCRIPT_SRC, rel};
+}
+inline CGNPath make_path_base_working(const std::string rel="")  { 
+    return CGNPath{CGNPath::BASE_ON_WORKINGROOT, rel};
+}
+
+} //namespace
+
+struct BinDevelOpt
+{
+    // const static int allow_bindevel    =     0b1;
+    // const static int allow_cxxinclude  =    0b10;
+    // const static int allow_linknrun    =  0b1100;
+    // const static int allow_output      = 0b10000;
+    
+    bool allow_bindevel    = false;
+    bool allow_cxxinclude  = false;
+    bool allow_linknrun    = false;
+    bool allow_output      = false;
+    
+    cgn::CGNPath target_dir = cgn::make_path_base_out();
+
+    std::string perferred_libdir;
+};
+
+struct FileUtility
+{
+    const std::string &name;
+    cgn::Configuration &cfg;
+
+    void copy_on_build(
+        const std::vector<std::string> &src, 
+        const cgn::CGNPath &src_base, 
+        const cgn::CGNPath &dst_dir
+    );
+
+    void flat_copy_on_build(
+        const std::vector<cgn::CGNPath> &src_list, 
+        const cgn::CGNPath &dst_dir
+    );
+
+    cgn::CGNTarget collect_devel_on_build(
+        const std::string &label, 
+        BinDevelOpt opt
+    );
+
+    // --TODO--
+    // cgn::CGNPath copy_rename_on_build(
+    //     const cgn::CGNPath &src_file,
+    //     const cgn::CGNPath &dst_file
+    // );
+    // cgn::CGNPath analysis_regex_file(
+    //     const cgn::CGNPath &template_file,
+    //     std::unordered_map<std::string, std::string> vars,
+    //     const cgn::CGNPath &out_file
+    // );
+    // std::vector<cgn::CGNPath> analysis_gen_pkgconfig();
+    // std::vector<cgn::CGNPath> analysis_gen_cmakeconfig();
+    // --END TODO--
+
+    std::vector<cgn::CGNPath> perferred_outputs;
+
+    FileUtility(cgn::CGNTargetOptIn *opt) 
+    : name(opt->factory_name), cfg(opt->cfg), opt(opt) {}
+
+private: friend class FileUtilityInterpreter;
+    cgn::CGNTargetOptIn *opt;
+
+    // copy_xxx()
+    struct CopyRecord {
+        const char *command;
+        std::string desc;
+        std::function<std::vector<std::string>(cgn::CGNTargetOpt *opt)> arg_gen;
+    };
+    std::vector<CopyRecord> copy_records;
+
+    // CGNTarget.infos<BinDevelInfo>
+    bool result_have_bin_devel = false;
+
+}; // struct FileUtility
+
+struct FileUtilityInterpreter
+{
+    using context_type = FileUtility;
+
+    constexpr static cgn::ConstLabelGroup<1> preload_labels() {
+        return {"@cgn.d//library/utility/file_utility.cgn.cc"};
+    }
+    CGN_UTILITY_API static void interpret(context_type &x);
+};
+
+#define file_utility(name, x) CGN_RULE_DEFINE(::FileUtilityInterpreter, name, x)
